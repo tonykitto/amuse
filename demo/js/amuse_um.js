fadcd707881d73a3232996fa16abffd48e074a26
tonykitto <tk@atoz.myzen.co.uk>
2015-01-09 18:23:10 +0000

    limits editor function

--- demo/js/amuse_um.js
+++ demo/js/amuse_um.js
@@ -1,13 +1,14 @@
 // support for simple web API
-// collects author initials for editing
+// if query is edit collects author initials for editing
+// else does not offer editor function
 var amuse_um = {
-	version : "4.4",
-	date : "2014-11-21",
-	session_init: function(coll, query){
+  version : "5.0",
+  date : "2014-12-29",
+  session_init: function(coll, query){
     "use strict";
     var event;
     if (window.EDIT && ("setup_EDIT" in window.EDIT)){
-      if (! query){
+      if (query === "edit"){
         window.VIEW.author = prompt("Add initials for editing");
         if (window.VIEW.author){
           window.VIEW.editor = window.EDIT.setup_EDIT;
@@ -16,12 +17,14 @@ var amuse_um = {
     }
     window.VIEW.file_name = coll;
     window.VIEW.start_VIEW(window[coll]);
-    if (query){
+    if (query && query !== "edit"){
       if (query in window[coll].objects){
         window.VIEW.display_object(query);
       }
-      else{
-        document.getElementById("filter_box").value = query.replace(/%20/g, " ").replace(/%22/g, "\"");
+      else{ // ~ is used at start of search string to indicate search for hash tag
+            // instead of # , which in a uri search string is a fragment identifier 
+        document.getElementById("filter_box").value = 
+          query.replace(/%20/g, " ").replace(/%22/g, "\"").replace(/^~/,"#");
         if (window.event){ event = window.event; }
         else{ event = {}; }
         event.keyCode = 13;
@@ -30,5 +33,5 @@ var amuse_um = {
       return "";
     }
     return "";
-	}
+  }
 };
